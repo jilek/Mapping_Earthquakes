6b874c254a8b570ada3c6b6fd27d1e2973fa1ba2
jilek <bjilek1@yahoo.com>
2021-05-25 12:16:25 -0500

    basic leaflet code

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -14,7 +14,38 @@ let map = L.map('mapid').setView([40.7, -94.5], 4);
 //     accessToken: API_KEY
 // });
 
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+// //  Add a marker to the map for Los Angeles, California.
+// let marker = L.marker([34.0522, -118.2437]).addTo(map);
+
+// Add a marker to the map for Los Angeles, CA
+// var marker = L.circle([34.0522, -118.2437], {
+//     radius: 1000,
+//     color: 'red',
+//     fillColor: '#f03',
+// }).addTo(map);
+// marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
+var marker = L.circleMarker([34.052, -118.2437], {
+    radius: 300,
+    color: "black",
+    fillColor: '#ffffa1'
+}).addTo(map);
+marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
+
+// function onMapClick(e) {
+//     alert("You clicked the map at " + e.latlng);
+// }
+// map.on('click', onMapClick);
+
+// var popup = L.popup();
+// function onMapClick(e) {
+//     popup
+//         .setLatLng(e.latlng)
+//         .setContent("You clicked the map at " + e.latlng.toString())
+//         .openOn(map);
+// }
+// map.on('click', onMapClick);
+
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
