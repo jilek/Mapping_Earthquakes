66e2a82625ed304d4e8c632563db0d794d7a5913
jilek <bjilek1@yahoo.com>
2021-05-27 10:27:40 -0500

    add GeoJSON data

--- Mapping_GeoJSON_Points/static/js/logic.js
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -1,67 +1,52 @@
-// Add console.log to check to see if our code is working.
-console.log("working");
 
-// Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([40.7, -94.5], 4);
+// Create the map object with center at the San Francisco airport.
+let map = L.map('mapid').setView([37.5, -122.5], 10);
 
-// Get data from cities.js
-let cityData = cities;
+// Add GeoJSON data.
+let sanFranAirport = {
+    "type": "FeatureCollection", "features": [
+        {
+            "type": "Feature",
+            "properties": {
+                "id": "3469",
+                "name": "San Francisco International Airport",
+                "city": "San Francisco",
+                "country": "United States",
+                "faa": "SFO",
+                "icao": "KSFO",
+                "alt": "13",
+                "tz-offset": "-8",
+                "dst": "A",
+                "tz": "America/Los_Angeles"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [-122.375, 37.61899948120117]
+            }
+        }
+    ]
+};
 
-// Loop through the cities array and create one marker for each city.
-cityData.forEach(function (city) {
-    console.log(city);
-    L.circleMarker(city.location, { radius: city.population / 100000 })
-        .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
-        .addTo(map);
-});
-
-console.log("still working");
-
-
-// We create the tile layer that will be the background of our map.
-// let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-//     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-//     maxZoom: 18,
-//     id: 'mapbox/streets-v11',
-//     tileSize: 512,
-//     zoomOffset: -1,
-//     accessToken: API_KEY
-// });
-
-// //  Add a marker to the map for Los Angeles, California.
-// let marker = L.marker([34.0522, -118.2437]).addTo(map);
-
-// Add a marker to the map for Los Angeles, CA
-// var marker = L.circle([34.0522, -118.2437], {
-//     radius: 1000,
-//     color: 'red',
-//     fillColor: '#f03',
+// Grabbing our GeoJSON data.
+// L.geoJson(sanFranAirport, {
+//     // We turn each feature into a marker on the map.
+//     pointToLayer: function (feature, latlng) {
+//         console.log(feature);
+//         return L.marker(latlng)
+//             .bindPopup("<h2>" + feature.properties.name + "</h2> <hr> </h3>"
+//                 + feature.properties.city + ", " + feature.properties.country + "</h3>");
+//     }
 // }).addTo(map);
-// marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
 
+L.geoJson(sanFranAirport, {
+    onEachFeature: function (feature, layer) {
+        console.log(layer);
+        layer.bindPopup("<h2>" + feature.properties.name + "</h2> <hr> </h3>"
+            + feature.properties.city + ", " + feature.properties.country + "</h3>");
+    }
+}).addTo(map);
 
-// var marker = L.circleMarker([34.052, -118.2437], {
-//     radius: 300,
-//     color: "black",
-//     fillColor: '#ffffa1'
-// }).addTo(map);
-// marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
-
-// function onMapClick(e) {
-//     alert("You clicked the map at " + e.latlng);
-// }
-// map.on('click', onMapClick);
-
-// var popup = L.popup();
-// function onMapClick(e) {
-//     popup
-//         .setLatLng(e.latlng)
-//         .setContent("You clicked the map at " + e.latlng.toString())
-//         .openOn(map);
-// }
-// map.on('click', onMapClick);
-
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/outdoors-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
