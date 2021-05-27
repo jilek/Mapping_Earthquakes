f75cadd4a4f020bd7cf81f9ad54c4c20d777b0fd
jilek <bjilek1@yahoo.com>
2021-05-27 16:03:31 -0500

    done with 13.5.5

--- Mapping_GeoJSON_Linestrings/static/js/logic.js
+++ Mapping_GeoJSON_Linestrings/static/js/logic.js
@@ -27,7 +27,7 @@ L.control.layers(baseMaps).addTo(map);
 // Accessing the airport GeoJSON URL
 //let airportData = "https://raw.githubusercontent.com/jilek/Mapping_Earthquakes/main/majorAirports.json";
 // Accessing the Toronto airline routes GeoJSON URL.
-let torontoData = "https://raw.githubusercontent.com/jilek/Mapping_Earthquakes/Mapping_GeoJSON_Linestrings/torontoRoutes.json";
+let torontoData = "https://raw.githubusercontent.com/jilek/Mapping_Earthquakes/Mapping_GeoJSON_Linestrings/Mapping_GeoJSON_Linestrings/torontoRoutes.json";
 
 // Grabbing our GeoJSON data.
 // d3.json(airportData).then(function (data) {
@@ -40,9 +40,22 @@ let torontoData = "https://raw.githubusercontent.com/jilek/Mapping_Earthquakes/M
 //         }
 //     }).addTo(map);
 // });
+
+// Create a style for the lines.
+let myStyle = {
+    color: "#ffffa1",
+    weight: 2
+};
+
 // Grabbing our GeoJSON data.
 d3.json(torontoData).then(function (data) {
     console.log(data);
     // Creating a GeoJSON layer with the retrieved data.
-    L.geoJson(data).addTo(map);
+    L.geoJson(data, {
+        style: myStyle,
+        onEachFeature: function (feature, layer) {
+            layer.bindPopup(",h3> Airline: " + feature.properties.airline + "</h3> <hr> <h3> Destination: "
+                + feature.properties.dst + "</h3>");
+        }
+    }).addTo(map);
 });
