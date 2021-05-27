ae7ba9b7a7a873aca2803fa8d3b7324463dc8705
jilek <bjilek1@yahoo.com>
2021-05-27 18:27:31 -0500

    done with 13.5.6

--- Mapping_GeoJSON_Polygons/static/js/logic.js
+++ Mapping_GeoJSON_Polygons/static/js/logic.js
@@ -10,24 +10,20 @@ let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/sate
     maxZoom: 18,
     accessToken: API_KEY
 });
-console.log("foo");
 
 let baseMaps = {
     Streets: streets,
     SatelliteStreets: satelliteStreets
 }
-console.log("bar");
 
 let map = L.map('mapid', {
     center: [43.7, -79.3],
     zoom: 11,
     layers: [streets]
 });
-console.log("baz");
 
 // Pass our map layers into our layers control and add the layers control to the map.
 L.control.layers(baseMaps).addTo(map);
-console.log("baz");
 
 // Accessing the Toronto airline routes GeoJSON URL.
 let torontoHoods = "https://raw.githubusercontent.com/jilek/Mapping_Earthquakes/Mapping_GeoJSON_Polygons/Mapping_GeoJSON_Polygons/torontoNeighborhoods.json";
@@ -42,12 +38,12 @@ let myStyle = {
 console.log("baz");
 // Grabbing our GeoJSON data.
 d3.json(torontoHoods).then(function (data) {
-    console.log(data, {
+    console.log(data);
+    L.geoJson(data, {
         style: myStyle,
         onEachFeature: function (feature, layer) {
-            layer.bindPopup("<h2>" + feature.properties.AREA_NAME + "</h2>");
+            layer.bindPopup("<h3> Neighborhood: " + feature.properties.AREA_NAME + "</h3>");
         }
-    });
-    // Creating a GeoJSON layer with the retrieved data.
-    L.geoJson(data).addTo(map);
+    }).addTo(map);
 });
+console.log("done");
