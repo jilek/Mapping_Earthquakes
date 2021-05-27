58a1b823b0450de0da17685d6515ffee0806de43
jilek <bjilek1@yahoo.com>
2021-05-27 17:24:29 -0500

    done with 13.5.6

--- Mapping_GeoJSON_Polygons/static/js/logic.js
+++ Mapping_GeoJSON_Polygons/static/js/logic.js
@@ -1,3 +1,4 @@
+
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
@@ -9,39 +10,44 @@ let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/sate
     maxZoom: 18,
     accessToken: API_KEY
 });
+console.log("foo");
 
 let baseMaps = {
-    Street: streets,
-    Satellite Streets: satelliteStreets
+    Streets: streets,
+    SatelliteStreets: satelliteStreets
 }
+console.log("bar");
 
 let map = L.map('mapid', {
     center: [43.7, -79.3],
-    zoom: 2,
-    layers: [satelliteStreets]
+    zoom: 11,
+    layers: [streets]
 });
+console.log("baz");
 
 // Pass our map layers into our layers control and add the layers control to the map.
 L.control.layers(baseMaps).addTo(map);
+console.log("baz");
 
 // Accessing the Toronto airline routes GeoJSON URL.
-let torontoHoods = "https://raw.githubusercontent.com/jilek/Mapping_Earthquakes/Mapping_GeoJSON_Polygons/Mapping_GeoJSON_Polygons/torontoRoutes.json";
+let torontoHoods = "https://raw.githubusercontent.com/jilek/Mapping_Earthquakes/Mapping_GeoJSON_Polygons/Mapping_GeoJSON_Polygons/torontoNeighborhoods.json";
 
 // Create a style for the lines.
 let myStyle = {
-    color: "#ffffa1",
-    weight: 2
+    color: "#0000ff",
+    fillColor: "#ffff00",
+    bubblingMouseEventa: true,
+    weight: 1
 };
-
+console.log("baz");
 // Grabbing our GeoJSON data.
 d3.json(torontoHoods).then(function (data) {
-    console.log(data);
-    // Creating a GeoJSON layer with the retrieved data.
-    L.geoJson(data, {
+    console.log(data, {
         style: myStyle,
         onEachFeature: function (feature, layer) {
-            layer.bindPopup(",h3> Airline: " + feature.properties.airline + "</h3> <hr> <h3> Destination: "
-                + feature.properties.dst + "</h3>");
+            layer.bindPopup("<h2>" + feature.properties.AREA_NAME + "</h2>");
         }
-    }).addTo(map);
+    });
+    // Creating a GeoJSON layer with the retrieved data.
+    L.geoJson(data).addTo(map);
 });
